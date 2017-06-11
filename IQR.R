3690620e2c1d7de8c21d5cdd8acdfe4696b3f585
Bhargav Krishna <bharkrish2014@outlook.com>
2017-06-11 12:13:04 -0500

    Create IQR.R

--- IQR.R
+++ IQR.R
@@ -1,14 +1,9 @@
-num<-readline(prompt="Enter array length")
-num<-as.integer(num)
-arr<-array(dim=num)
-for(i in 1:num){
-  elem<-readline(prompt="Enter array elements")
-  arr[i]<-as.numeric(elem)
-}
-
+f <- file("stdin")
+open(f)
+data <- strsplit(readLines(f, warn=FALSE), split=" ")
 prepare_arr<-function(x,y){
-res_arr<-array(dim=sum(freq))
-  index<-1
+    res_arr<-array(dim=sum(y))
+    index<-1
     for(j in 1:length(x)){
       for(k in 1:y[j]){
         res_arr[index]<-x[j]
@@ -17,20 +12,13 @@ res_arr<-array(dim=sum(freq))
     }
   return (res_arr)
 }
-
-
-f <- file("stdin")
-open(f)
-d <- strsplit(readLines(f, warn=FALSE), split=" ")
-
-
 quartile<-function(x){
   x<-sort(x)
   len<-length(x)
   mid<-(len+1)/2
   ifelse(floor(mid)==mid, {low<-mid-1;up<-mid+1}, {low<-mid-0.5; up<-mid+0.5})
-  return (c(median(x[1:low]), median(x), median(x[up:len])))
+  return (c(median(x[1:low]), median(x[up:len])))
 }
-prepared_data<-prepare_arr(elements, freq)
+prepared_data<-prepare_arr(as.numeric(data[[2]]), as.numeric(data[[3]]))
 quart<-quartile(prepared_data)
-cat(quart[3]-quart[1], "\n")
+cat(format(round(quart[2]-quart[1], digits=1), nsmall=1), "\n")
