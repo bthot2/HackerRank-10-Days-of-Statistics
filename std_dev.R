9b1154c785481a3a12e5457380132f7d1d4284f8
Bhargav Krishna <bharkrish2014@outlook.com>
2017-06-11 12:14:45 -0500

    Create std_dev.R

--- std_dev.R
+++ std_dev.R
@@ -1,10 +1,8 @@
-num<-readline(prompt="Enter array length")
-num<-as.integer(num)
-arr<-array(dim=num)
-for(i in 1:num){
-  elem<-readline(prompt="Enter array elements")
-  arr[i]<-as.numeric(elem)
-}
+f <- file("stdin")
+open(f)
+d <- strsplit(readLines(f, warn=FALSE), split=" ")
+num<-as.numeric(d[[1]])
+arr<-array(as.numeric(d[[2]]), dim=length(d[[2]]))
 sqr_arr<-apply(arr, FUN = function(x){(x-mean(arr))^2},
                MARGIN = c(1))
-sqrt(sum(sqr_arr)/5)
+cat(sqrt(sum(sqr_arr)/num))
