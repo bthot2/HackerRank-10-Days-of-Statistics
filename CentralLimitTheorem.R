5fe5c251ad5251168d0fda112c171593aba0c433
Bhargav Krishna <bharkrish2014@outlook.com>
2017-06-11 12:32:27 -0500

    Create CentralLimitTheorem.R

--- CentralLimitTheorem.R
+++ CentralLimitTheorem.R
@@ -11,3 +11,15 @@ data<-as.numeric(unlist(data))
 prob<-pnorm(mean=data[3]*data[2], sd = data[4]*(sqrt(data[2])), data[1])
 cat(format(round(prob, digits=4), nsmall=1), "\n")
 
+#Central Limit Theorem-III
+data <- strsplit(readLines(file('stdin'), warn=FALSE), split="\n")
+data<-as.numeric(unlist(data))
+mean<-data[2]
+stddev<-data[3]
+n<-data[1]
+z<-data[5]
+lower<-mean-(z*(stddev/sqrt(n)))
+upper<-mean+(z*(stddev/sqrt(n)))
+cat(format(round(lower, digits=2), nsmall=1), "\n")
+cat(format(round(upper, digits=2), nsmall=1), "\n")
+
