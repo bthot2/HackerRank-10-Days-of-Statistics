5826dd59f8b773f21ad48f8f7248521868d28ab6
Bhargav Krishna <bharkrish2014@outlook.com>
2017-06-11 12:31:34 -0500

    Create CentralLimitTheorem.R

--- CentralLimitTheorem.R
+++ CentralLimitTheorem.R
@@ -1,6 +1,13 @@
+#Central Limit Theorem-I
 data <- strsplit(readLines(file('stdin'), warn=FALSE), split="\n")
 data<-as.numeric(unlist(data))
 data<-as.numeric(data)
+prob<-pnorm(mean=data[3]*data[2], sd = data[4]*(sqrt(data[2])), data[1])
+cat(format(round(prob, digits=4), nsmall=1), "\n")
+
+#Central Limit Theorem-II
+data <- strsplit(readLines(file('stdin'), warn=FALSE), split=" ")
+data<-as.numeric(unlist(data))
 prob<-pnorm(mean=data[3]*data[2], sd = data[4]*(sqrt(data[2])), data[1])
 cat(format(round(prob, digits=4), nsmall=1), "\n")
 
