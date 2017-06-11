5bef678903d014c96021309755eced9406842e01
Bhargav Krishna <bharkrish2014@outlook.com>
2017-06-11 12:11:38 -0500

    Update Quartiles.R

--- Quartiles.R
+++ Quartiles.R
@@ -1,11 +1,8 @@
-num<-readline(prompt="Enter array length")
-num<-as.integer(num)
-arr<-array(dim=num)
-for(i in 1:num){
-  elem<-readline(prompt="Enter array elements")
-  arr[i]<-as.numeric(elem)
-}
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+data <- read.table("stdin", header = FALSE, skip = 1,sep="")
 
+### transpose the table to be able to use mean() and median()
+data <- t(data)
 quartile<-function(x){
   x<-sort(x)
   len<-length(x)
@@ -13,10 +10,7 @@ quartile<-function(x){
   ifelse(floor(mid)==mid, {low<-mid-1;up<-mid+1}, {low<-mid-0.5; up<-mid+0.5})
   return (c(median(x[1:low]), median(x), median(x[up:len])))
 }
-quart<-quartile(arr)
-#or
-result<-quantile(sort(arr), probs=c(0.25,0.5,0.75), type=6)
-
-for(i in 1:length(result))
-  cat(quart[i], "\n")
+quart<-quartile(data)
+for(i in 1:length(quart))
+    cat(quart[i], "\n")
 
